efe7102671a11993d77ab0ac8b5da094da2568de
Natesworks <contact@natesworks.com>
2024-07-13 13:08:44 +0200

    Add getPackageInfo to header

--- frontend.h
+++ frontend.h
@@ -3,5 +3,6 @@
 int main(int argc, char *argv[]);
 int install(std::string package);
 int uninstall(std::string package);
+int getPackageInfo(std::string package);
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
 int login(const std::string& username, const std::string& password);
